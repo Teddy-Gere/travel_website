9a58618e2eea05b503214c58c8c4db3182ac7d46
kalayu-ftsum <ftsumkalayu12@gmail.com>
2023-01-26 13:54:10 +0100

    made header fixed to the top

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,14 @@ menuButton.addEventListener('click',menuHandler)
 closeButton.addEventListener('click',menuHandler)
 
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
 
 
 /*==================== SWIPER DISCOVER ====================*/
